336f5c92f1817ed64b656706a562c077070ee4b1
Jungkeun Lee <lee.jungkeun@gmail.com>
2020-05-06 10:30:52 -0400

    first commit

---
+++ freopen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+
+    printf("This text is redirected to stdout\n");
+
+    fp = freopen("file.txt", "w+", stdout);
+
+    printf("This text is redirected to file.txt\n");
+
+    fclose(fp);
+
+    return 0;
+}
---
+++ temp.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a = 123;
+    printf("%d%n\n", a, &a);
+    return 0;
+}

12dcb4ced7568d21a20c6dffe7358f61c0fb3226
Jungkeun Lee <lee.jungkeun@gmail.com>
2020-05-06 23:48:11 -0400

    WedMay6-2

--- temp.c
+++ temp.c
@@ -1,8 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int main(void)
 {
-    int a = 123;
-    printf("%d%n\n", a, &a);
+    FILE *fp;
+    int num_parts = 3;
+    int a[] = {1, 2, 3};
+
+    if((fp = fopen("temp.txt", "w")) == NULL) {
+        fprintf(stderr, "Can't open inventory file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < num_parts; i++)
+        printf("%d\n", a[i]);
+
+    fwrite(a, sizeof(int), num_parts, fp);
+    fclose(fp);
+
     return 0;
 }

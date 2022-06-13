6d7027d94bc1e824ab2cbba1dff783e936298497
Jungkeun Lee <lee.jungkeun@gmail.com>
2022-06-12 23:15:56 -0400

    2022-6-12

---
+++ knking/length.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int len = 0;
+  printf("Enter a message: ");
+  while (getchar() != '\n')
+    len++;
+  printf("Your message was %d charcter(s) long.\n", len);
+
+  return 0;
+}
--- knking/temp.c
+++ knking/temp.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-  int num, d;
-  printf("Enter the number: ");
-  scanf("%d", &num);
-
-  for (d = 2; d * d <= num; d++) {
-    if (num % d == 0) {
-      printf("This in not a prime number.\n");
-      return 0;
-    }
-  }
-  printf("This is a prime number\n");
+  printf("%zu\n", sizeof(int));
   return 0;
 }

24278f75b5ed45ef9cf2e8de8f1b4c3ca5a1de80
Jungkeun Lee <jungkeun@duck.com>
2022-07-14 21:21:34 -0400

    2022-7-14-linux#1

--- knking/temp.c
+++ knking/temp.c
@@ -1,8 +1,25 @@
-#include <math.h>
 #include <stdio.h>
 
 int main(void) {
-  double i = 3, j = 5;
-  printf("%lf\n", pow(i, j));
+  int rank[5] = {1, 3, 3, 3, 3};
+
+  int index = 0;
+  int result = 0;
+  int sum;
+
+  while (index < 4) {
+    sum = 0;
+    while (rank[index] == rank[index + 1]) {
+      sum += 1;
+      if (index == 3)
+        break;
+      else
+        index++;
+    }
+    index++;
+    result += sum * sum;
+  }
+
+  printf("%d\n", result);
   return 0;
 }
